7e4e7962d8cfdc540218dd2f7d112ead041911d1
Ariel Ivan Diaz <ariel@laguna.com.ar>
2023-05-20 18:52:40 -0300

    init vite

---
+++ src/components/component1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const component1 = () => {
+
+    let name "Ariel Ivan Diaz";
+    let company = "Laguna";
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
